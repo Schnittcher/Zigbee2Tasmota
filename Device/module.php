5f0e302010aab1f424e70a79cccf09bea3a0788c
Kai Schnittcher <kai.schnittcher@gmail.com>
2020-08-09 03:14:11 +0200

    Device mit Templates konfigurierbar gemacht

--- Device/module.php
+++ Device/module.php
@@ -1,18 +1,12 @@
 <?php
 
-
 declare(strict_types=1);
 
-if (!function_exists('fnmatch')) {
-    function fnmatch($pattern, $string)
-    {
-        return preg_match('#^' . strtr(preg_quote($pattern, '#'), ['\*' => '.*', '\?' => '.']) . '$#i', $string);
-    }
-}
-
 require_once __DIR__ . '/../libs/VariableProfileHelper.php';
+require_once __DIR__ . '/../libs/Functions.php';
+require_once __DIR__ . '/../libs/Devices.php';
 
-class Tasmota2ZigbeeDevice extends IPSModule
+class Tasmota2ZigbeeDevice extends Devices
 {
     use VariableProfileHelper;
 
@@ -26,6 +20,7 @@ class Tasmota2ZigbeeDevice extends IPSModule
 
         //Anzahl die in der Konfirgurationsform angezeigt wird - Hier Standard auf 1
         $this->RegisterPropertyString('Device', '');
+        $this->RegisterPropertyString('Model', '');
     }
 
     public function ApplyChanges()
@@ -39,6 +34,34 @@ class Tasmota2ZigbeeDevice extends IPSModule
         $this->SendDebug('SetReceiveDataFilter - Device', $device, 0);
 
         $this->SetReceiveDataFilter('.*' . $device . '.*');
+
+        $model = $this->ReadPropertyString('Model');
+
+        if (array_key_exists($model, self::$Devices)) {
+            foreach (self::$Devices[$model] as $key => $device) {
+                switch ($device['VariableType']) {
+                case IPSVarType::vtBoolean:
+                    $this->RegisterVariableBoolean($key, $this->Translate($device['Name']), $device['VariableProfile']);
+                    break;
+                case IPSVarType::vtInteger:
+                    $this->RegisterVariableInteger($key, $this->Translate($device['Name']), $device['VariableProfile']);
+                    break;
+                case IPSVarType::vtFloat:
+                    $this->RegisterVariableFloat($key, $this->Translate($device['Name']), $device['VariableProfile']);
+                    break;
+                case IPSVarType::vtString:
+                    $this->RegisterVariableString($key, $this->Translate($device['Name']), $device['VariableProfile']);
+                    break;
+                default:
+                    $this->SendDebug('unknown VariableType', $model . ': ' . $key, 0);
+               }
+                if ($device['Action']) {
+                    $this->EnableAction($key);
+                }
+            }
+        } else {
+            $this->SendDebug('unknown model', $model, 0);
+        }
     }
 
     public function ReceiveData($JSONString)
@@ -48,34 +71,69 @@ class Tasmota2ZigbeeDevice extends IPSModule
         if (!empty($this->ReadPropertyString('Device'))) {
             $data = json_decode($JSONString);
             $Buffer = $data->Buffer;
-            
+
             if (property_exists($Buffer, 'Topic')) {
                 if (fnmatch('*/SENSOR', $Buffer->Topic)) {
                     //$this->SendDebug('Topic: Result Payload', $Buffer->Payload, 0);
                     $Payload = json_decode($Buffer->Payload)->ZbReceived->{$device};
                     if (is_object($Payload)) {
-                        if (property_exists($Payload, 'Power')) {
-                            $this->RegisterVariableInteger('Power', $this->Translate('Power'), 'T2M.Power');
-                            SetValue($this->GetIDForIdent('Power'), $Payload->Power);
+                        $model = $this->ReadPropertyString('Model');
+                        foreach (self::$Devices[$model] as $key => $device) {
+                            if (property_exists($Payload, $device['SearchString'])) {
+                                if ($key == $device['SearchString']) {
+                                    $this->SetValue($key, $Payload->{$key});
+                                } else {
+                                    switch ($device['VariableType']) {
+                                        case IPSVarType::vtBoolean:
+                                            $this->SetValue($key, true);
+                                    }
+                                }
+                            }
                         }
-                        if (property_exist($Payload,'0006!42')) {
-                            $this->RegisterVariableInteger('Power', $this->Translate('Power'), 'T2M.Power');
-                            SetValue($this->GetIDForIdent('Power'), 1);
-                        }
+
+                        /**                         if (property_exists($Payload, 'Power')) {
+                         * SetValue($this->GetIDForIdent('Power'), $Payload->Power);
+                         * }
+                         *
+                         * if (property_exist($Payload, '0006!42')) {
+                         * $this->RegisterVariableInteger('Power', $this->Translate('Power'), 'T2M.Power');
+                         * SetValue($this->GetIDForIdent('Power'), 1);
+                         * }
+                         */
                     }
                 }
             }
         }
     }
 
+    public function RequestAction($Ident, $Value)
+    {
+        $model = $this->ReadPropertyString('Model');
+        $Command = self::$Devices[$model][$Ident]['ActionCommand'];
+
+        $Data['DataID'] = '{91D0FFCD-72C7-EDD1-8525-4348DAD309BA}';
+        $Buffer['Topic'] = 'ZbSend';
+
+        $ZbSend['device'] = $this->ReadPropertyString('Device');
+        $ZbSend['send'][$Ident] = $Value;
+
+        $Buffer['Payload'] = json_encode($ZbSend);
+        $Data['Buffer'] = json_encode($Buffer);
+
+        $this->SendDebug('JSON Reload Devices', json_encode($Data), 0);
+        $this->SendDataToParent(json_encode($Data));
+
+        $this->SendDebug('Action', $Command, 0);
+    }
+
     private function registerVariables()
     {
-        if (!IPS_VariableProfileExists('T2M.Power')) {
+        if (!IPS_VariableProfileExists('T2M.TogglePower')) {
             $Associations = [];
             $Associations[] = [0, $this->Translate('Off'), '', -1];
             $Associations[] = [1, $this->Translate('On'), '', -1];
             $Associations[] = [2, $this->Translate('Toggle'), '', -1];
-            $this->RegisterProfileIntegerEx('T2M.Power', '', '', '', $Associations);
+            $this->RegisterProfileIntegerEx('T2M.TogglePower', '', '', '', $Associations);
         }
     }
 }
---
+++ libs/Devices.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+class IPSVarType
+{
+    const vtNone = -1;
+    const vtBoolean = 0;
+    const vtInteger = 1;
+    const vtFloat = 2;
+    const vtString = 3;
+    const vtDualInteger = 10;
+}
+
+class Devices extends IPSModule
+{
+    protected static $Devices = [
+
+        'OSRAM Plug 01' => [
+            'Power' => [
+                'Name'                   => 'Power',
+                'VariableProfile'        => '~Switch',
+                'VariableType'           => IPSVarType::vtBoolean,
+                'Action'                 => true,
+                'ActionCommand'          => 'Power',
+                'SearchString'           => ''
+            ]
+        ],
+        'IKEA of Sweden TRADFRI bulb E27 CWS opal 600lm' => [
+            'Power' => [
+                'Name'                   => 'Power',
+                'VariableProfile'        => '~Switch',
+                'VariableType'           => IPSVarType::vtBoolean,
+                'Action'                 => true,
+                'ActionCommand'          => 'Power',
+                'SearchString'           => 'Power'
+            ],
+            'Dimmer' => [
+                'Name'                   => 'Brightness',
+                'VariableProfile'        => '~Intensity.100',
+                'VariableType'           => IPSVarType::vtInteger,
+                'Action'                 => true,
+                'ActionCommand'          => 'Dimmer',
+                'SearchString'           => 'Dimmer'
+            ]
+            ],
+            'IKEA of Sweden TRADFRI on/off switch' => [
+                'Power' => [
+                    'Name'                   => 'Power',
+                    'VariableProfile'        => 'T2M.TogglePower',
+                    'VariableType'           => IPSVarType::vtInteger,
+                    'Action'                 => false,
+                    'ActionCommand'          => '',
+                    'SearchString'           => 'Power'
+                ]
+            ],
+            'IKEA of Sweden TRADFRI motion sensor' => [
+                'Motion' => [
+                    'Name'                   => 'Motion',
+                    'VariableProfile'        => '~Motion',
+                    'VariableType'           => IPSVarType::vtBoolean,
+                    'Action'                 => false,
+                    'ActionCommand'          => '',
+                    'SearchString'           => '0006!42'
+                ]
+            ],
+            'LUMI lumi.sensor_wleak.aq1' => [
+                'Alarm ' => [
+                    'Name'                   => '~Alert',
+                    'VariableProfile'        => '',
+                    'VariableType'           => IPSVarType::vtBoolean,
+                    'Action'                 => false,
+                    'ActionCommand'          => '',
+                    'SearchString'           => 'ZoneStatusChange'
+                ]
+            ],
+            'LUMI lumi.sensor_magnet' => [
+                'Alarm ' => [
+                    'Name'                   => '~Window',
+                    'VariableProfile'        => '',
+                    'VariableType'           => IPSVarType::vtBoolean,
+                    'Action'                 => false,
+                    'ActionCommand'          => '',
+                    'SearchString'           => 'Power'
+                ]
+            ],
+    ];
+}
