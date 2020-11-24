749db18722eb8434bac31da592b8995e67295ead
Kai Schnittcher <kai.schnittcher@gmail.com>
2020-11-24 09:30:37 +0100

    Gardenpole RGBW-Lightify, Philips LCT015, Philips LCT001 hinzugefügt

--- libs/Devices/osram.php
+++ libs/Devices/osram.php
@@ -12,5 +12,39 @@ return [
             'ActionCommand'          => 'Power',
             'SearchString'           => 'Power'
         ]
+    ],
+    'Gardenpole RGBW-Lightify' => [
+        'Power' => [
+            'Name'                   => 'State',
+            'VariableProfile'        => '~Switch',
+            'VariableType'           => VARIABLETYPE_BOOLEAN,
+            'Action'                 => true,
+            'ActionCommand'          => 'Power',
+            'SearchString'           => 'Power'
+        ],
+        'Dimmer' => [
+            'Name'                   => 'Brightness',
+            'VariableProfile'        => '~Intensity.255',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Dimmer',
+            'SearchString'           => 'Dimmer'
+        ],
+        'Color' => [
+            'Name'                   => 'Color',
+            'VariableProfile'        => 'HexColor',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Color',
+            'SearchString'           => 'X'
+        ],
+        'CT' => [
+            'Name'                   => 'Color Temperature',
+            'VariableProfile'        => 'T2M.ColorTemperature',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'CT',
+            'SearchString'           => 'CT'
+        ],
     ]
 ];
--- libs/Devices/philips.php
+++ libs/Devices/philips.php
@@ -36,5 +36,73 @@ return [
             'ActionCommand'          => 'CT',
             'SearchString'           => 'CT'
         ],
+    ],
+    'LCT007' => [
+        'Power' => [
+            'Name'                   => 'State',
+            'VariableProfile'        => '~Switch',
+            'VariableType'           => VARIABLETYPE_BOOLEAN,
+            'Action'                 => true,
+            'ActionCommand'          => 'Power',
+            'SearchString'           => 'Power'
+        ],
+        'Dimmer' => [
+            'Name'                   => 'Brightness',
+            'VariableProfile'        => '~Intensity.255',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Dimmer',
+            'SearchString'           => 'Dimmer'
+        ],
+        'Color' => [
+            'Name'                   => 'Color',
+            'VariableProfile'        => 'HexColor',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Color',
+            'SearchString'           => 'X'
+        ],
+        'CT' => [
+            'Name'                   => 'Color Temperature',
+            'VariableProfile'        => 'T2M.ColorTemperature',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'CT',
+            'SearchString'           => 'CT'
+        ],
+    ],
+    'LCT001' => [
+        'Power' => [
+            'Name'                   => 'State',
+            'VariableProfile'        => '~Switch',
+            'VariableType'           => VARIABLETYPE_BOOLEAN,
+            'Action'                 => true,
+            'ActionCommand'          => 'Power',
+            'SearchString'           => 'Power'
+        ],
+        'Dimmer' => [
+            'Name'                   => 'Brightness',
+            'VariableProfile'        => '~Intensity.255',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Dimmer',
+            'SearchString'           => 'Dimmer'
+        ],
+        'Color' => [
+            'Name'                   => 'Color',
+            'VariableProfile'        => 'HexColor',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Color',
+            'SearchString'           => 'X'
+        ],
+        'CT' => [
+            'Name'                   => 'Color Temperature',
+            'VariableProfile'        => 'T2M.ColorTemperature',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'CT',
+            'SearchString'           => 'CT'
+        ],
     ]
 ];
