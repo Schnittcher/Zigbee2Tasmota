b7d4c2ad2e0ce8bb304ec22d8156845eeab60b09
Kai Schnittcher <kai.schnittcher@gmail.com>
2020-12-02 15:40:56 +0100

    IKEA zu Tradfri verschoben

--- libs/Devices/tradfri.php
+++ libs/Devices/tradfri.php
@@ -135,4 +135,30 @@ return [
             'SearchString'           => 'CT'
         ],
     ],
+    'TS0502A' => [
+        'Power' => [
+            'Name'                   => 'State',
+            'VariableProfile'        => '~Switch',
+            'VariableType'           => VARIABLETYPE_BOOLEAN,
+            'Action'                 => true,
+            'ActionCommand'          => 'Power',
+            'SearchString'           => 'Power'
+        ],
+        'Dimmer' => [
+            'Name'                   => 'Brightness',
+            'VariableProfile'        => '~Intensity.255',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Dimmer',
+            'SearchString'           => 'Dimmer'
+        ],
+        'CT' => [
+            'Name'                   => 'Color Temperature',
+            'VariableProfile'        => 'T2M.ColorTemperature',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'CT',
+            'SearchString'           => 'CT'
+        ],
+    ]
 ];
