536a24ea5292c6fe07f4a8b4e88cc03bbf53c0f1
Kai Schnittcher <kai.schnittcher@gmail.com>
2020-12-14 21:01:59 +0100

    Classic A60 RGBW hinzugefügt

--- libs/Devices/osram.php
+++ libs/Devices/osram.php
@@ -64,5 +64,39 @@ return [
             'ActionCommand'          => 'Dimmer',
             'SearchString'           => 'Dimmer'
         ]
+    ],
+    'Classic A60 RGBW' => [
+        'Power' => [
+            'Name'                   => 'State',
+            'VariableProfile'        => '~Switch',
+            'VariableType'           => VARIABLETYPE_BOOLEAN,
+            'Action'                 => true,
+            'ActionCommand'          => 'Power',
+            'SearchString'           => 'Power'
+        ],
+        'Dimmer' => [
+            'Name'                   => 'Brightness',
+            'VariableProfile'        => '~Intensity.255',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Dimmer',
+            'SearchString'           => 'Dimmer'
+        ],
+        'Color' => [
+            'Name'                   => 'Color',
+            'VariableProfile'        => 'HexColor',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'Color',
+            'SearchString'           => 'X'
+        ],
+        'CT' => [
+            'Name'                   => 'Color Temperature',
+            'VariableProfile'        => 'T2M.ColorTemperature',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => true,
+            'ActionCommand'          => 'CT',
+            'SearchString'           => 'CT'
+        ]
     ]
 ];
