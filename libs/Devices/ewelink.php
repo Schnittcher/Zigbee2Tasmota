93a58edbc293727732d17ddd8979dbd8ce60d261
Kai Schnittcher <kai.schnittcher@gmail.com>
2020-12-22 16:31:27 +0100

    eWeLink DS01 hinzugefügt

---
+++ libs/Devices/ewelink.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+return [
+    'DS01' => [
+        'Occupancy' => [
+            'Name'                   => 'Occupancy',
+            'VariableProfile'        => '~Motion',
+            'VariableType'           => VARIABLETYPE_BOOLEAN,
+            'Action'                 => false,
+            'ActionCommand'          => '',
+            'SearchString'           => 'Occupancy'
+        ]
+    ],
+];
