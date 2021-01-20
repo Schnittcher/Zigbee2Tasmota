ceb31e80ef0fc3133107b471ff3dbcdd754610b5
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-01-20 17:30:40 +0100

    Fix Key

--- libs/Devices/sonoff.php
+++ libs/Devices/sonoff.php
@@ -21,7 +21,7 @@ return [
             'SearchString'           => 'Humidity'
         ],
     ],
-    66666 => [
+    'Z2TSymcon-66666' => [
         'Temperature' => [
             'Name'                   => 'Temperature',
             'VariableProfile'        => '~Temperature',
